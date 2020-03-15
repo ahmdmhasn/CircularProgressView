2b96af965903f827d31a5ea77bd7669862727acf
Ahmed M. Hassan <ahassan@victorylink.com>
2020-03-15 14:36:08 +0200

    Initial Commit

---
+++ CircularProgressViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircularProgressViewDemo
+//
+//  Created by Ahmed M. Hassan on 3/15/20.
+//  Copyright © 2020 Ahmed M. Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
